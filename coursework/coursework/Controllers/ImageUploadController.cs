5a016329c5d34bb1ac797438912a045150850906
kost0v <kostovarov2015@gmail.com>
2024-09-27 16:17:41 +0300

    UpLoad

---
+++ coursework/coursework/Controllers/ImageUploadController.cs
@@ -0,0 +1,30 @@
+﻿// ImageUploadController.cs
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace coursework.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageUploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file uploaded");
+
+            var filePath = Path.Combine("wwwroot/images", file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var relativePath = $"/images/{file.FileName}";
+            return Ok(new { filePath = relativePath });
+        }
+    }
+}
---
+++ coursework/coursework/Models/RegisterModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace coursework.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
